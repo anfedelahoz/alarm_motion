bad2dc2ff8dacccf38b5e64838c769509c6b05a5
Andres De la Hoz <andelahozg@gmail.com>
2023-03-11 18:37:56 -0500

    alarm edit view created

--- Alarm_Motion_Web/alarmMotion/src/app/alarm/alarm.module.ts
+++ Alarm_Motion_Web/alarmMotion/src/app/alarm/alarm.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlarmCreateComponent } from './alarm-create/alarm-create.component';
 import { AlarmListComponent } from './alarm-list/alarm-list.component';
+import { AlarmEditComponent } from './alarm-edit/alarm-edit.component';
 
 
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [AlarmCreateComponent, AlarmListComponent],
-  exports: [AlarmCreateComponent, AlarmListComponent]
+  declarations: [AlarmCreateComponent, AlarmListComponent, AlarmEditComponent],
+  exports: [AlarmCreateComponent, AlarmListComponent, AlarmEditComponent]
 })
 export class AlarmModule { }
