16aca8bc4b53f30d509e9ffe08483dbdd7ff816f
Andres De la Hoz <andelahozg@gmail.com>
2023-03-11 19:53:19 -0500

    alarm confirmation view created

--- Alarm_Motion_Web/alarmMotion/src/app/alarm/alarm.module.ts
+++ Alarm_Motion_Web/alarmMotion/src/app/alarm/alarm.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AlarmCreateComponent } from './alarm-create/alarm-create.component';
 import { AlarmListComponent } from './alarm-list/alarm-list.component';
 import { AlarmEditComponent } from './alarm-edit/alarm-edit.component';
+import { AlarmConfirmationComponent } from './alarm-confirmation/alarm-confirmation.component';
 
 
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [AlarmCreateComponent, AlarmListComponent, AlarmEditComponent],
-  exports: [AlarmCreateComponent, AlarmListComponent, AlarmEditComponent]
+  declarations: [AlarmCreateComponent, AlarmListComponent, AlarmEditComponent, AlarmConfirmationComponent],
+  exports: [AlarmCreateComponent, AlarmListComponent, AlarmEditComponent, AlarmConfirmationComponent]
 })
 export class AlarmModule { }
